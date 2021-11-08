cc7ba3cd394c42b285ae1fe7cf158b5d14701e44
Phensic <juliusayoola5@gmail.com>
2021-11-09 00:09:50 +0100

    Object... completed

--- Object-Oriented JavaScript/OOP-Basic.js
+++ Object-Oriented JavaScript/OOP-Basic.js
@@ -27,126 +27,236 @@
 
 
 console.log("Hello World!");
-
 // OBJECTS
-// OBJECT LITERALS
+// OBJECT LITERALS 
+// => CREATION OBJECTS WITH AN OBJECT LITERAL SYNTAX NOT ADVISABLE
+// WHEN THE OBJECT HAS A BEHAVIOUR (FUNCTION) USE THE FACTORY AND CONSTRUCTOR
+const object = {
+    radius: 1,
+    location: {
+        x: 1,
+        y: 1
+    },
+    draw: function () {
+        console.log('draw');
+    }
+};
+object.draw();
 
+
+//THE CONSTRUCTOR PROPERT => REFERENCES THE FUNCTION THAT WAS USED TO CREATE THE OBJECT
 
 // FACTORY FUNCTION
-function createCircle(radius) {
-    return{
+function createObject(radius) {
+    return {
         radius,
-       draw: function() {
-           console.log('draw');
-       }
-    };
-}
-let circle = createCircle(10);
-circle.draw();
+        draw: function () {
+            console.log('draw');
+        }
+    }
+};
+const object2 = createObject(1);
 
 
 // CONSTRUCTOR FUNCTION
 function Circle(radius) {
     this.radius = radius;
-    this.draw = function () {
-    console.log('draw');
-    }
+    this.draw =function() {
+        console.log(draw);
+    } 
 }
+const another = new Circle(1);    //USING NEW OR
+Circle.call({}, 1)                // CALL ARE THE SAME THING
+Circle.apply({}, [1, 2, 3, 4])    //OR USE APPLY IF THE 2ND ARGUMENT IS AN ARRAY
+
+
 // FUNCTIONS ARE OBJECTS
-
-const Circle1 = new Function('radius',`
-    this.radius = radius;
-    this.draw = function () {
-        console.log('draw');
-    }`
-    );
-
-const another = new Circle(1);
-
-
-Circle.call({}, 1);
-Circle.apply({}, [1, 2, 3, 4]);
- 
-// CONSTRUCTOR PROPERTY: e.g(another.constructor)
-// EVERY OBJECT HAS A CONSTRUCTOR PROPERTY THAT REFERENCES THE 
-// FUNCTION THAT WAS USED TO CREATE THE OBJECT 
-
+const Circle1 = new Function('radius', `
+        this.radius = radius;
+        this.draw = function() {
+            console.log(draw);
+        }
+    `)
+const circle = new Circle1(1);
 
 
 // VALUE vs REFERENCE TYPES
-// VALUE TYPES => NUMBERS, STRINGS, BOOLEANS, SYMBOLS, UNDEFINED, NULL
-// REFERENCE TYPES => OBJECTS, FUNCTIONS, ARRAYS
 
-let x = {value: 10}
+// VALUE/PRIMITIVE TYPES => NUMBERS, STRINGS, BOOLEANS, SYMBOLS, UNDEFINED, NULL
+//=>  PRIMITIVES ARE COPIED BY THIER VALUES
+//=> AND ARE INDEPENDENT TO ONE ANOTHER
+let x = 400;
 let y = x;
 
-x.value = 20;
-//  PRIMITIVES ARE COPIED BY THIER VALUES
-//  OBJECTS ARE COPIED BY THEIR REFERENCES
-let number = 10;
+x = 200
 
+
+
+// REFERENCE/OBJECT TYPES => OBJECTS, FUNCTIONS, ARRAYS
+//=>  OBJECTS ARE COPIED BY THEIR REFERENCES
+//=> AND ARE DEPENDENT
+
+let p = {value: 10};
+let q = p
+
+p.value = 20
+
+// OR ANOTHER EXAMPLE IS:
+
+let obj = {value:10 };
 function increase(number) {
-    number++    
+    obj.value++;
 }
 
-increase(number);
-console.log(number);
+increase(obj);
+console.log(obj);
+
+
+
 
 // ADDING/REMOVING PROPERTIES
+//=> OBJECTS ARE DYNAMIC
 
 function Circular(radius) {
-    this.radius = radius;
+    this.radius = radius
     this.draw = function() {
         console.log('draw');
     }
 }
+
 const circular = new Circular(10);
-circular.location =  {x: 1 };
 
-const propertyName = "location";
+circular.location = { x: 200 };        // THE DOT NOTATION 
 
-circular[propertyName] = {x: 1 };
 
-// delete circular['location'];
+const propertyName = 'location';
+circular['propertyName'] = { x: 1 };     // AND THE BRACKET NOTATION ARE SAME
+//=> THE BRACKET NOTATION ARE USED WITH PROPERTY NAMES THAT ARE NOT VALID IDETIFYERS (-,SPACE)
 
-// ENUMERATING PROPERTIES
-for (let key in circle) {
-    if (typeof circle[key !== 'function'])
-    console.log(key, circle[key]);
+// DELETING PROPERIES
+// delete circular.location;
+
+
+// ENUMERATING PROPERTIES(FOR-IN LOOP)
+for (let key in circular) {       //ENUMERATING OBJECT MEMBERS
+    if (typeof circular[key] !== 'function')   //
+    console.log(key, circular[key]); 
 }
-
-
-const keys = Object.keys(circle)
+//  OR ANOTHER APPROACH CAN BE THIS TO GET ALL THE KEYS
+const keys = Object.keys(circular);  //BUT CANNOT SEPERATE PROPERTIES FROM METHODS  
 console.log(keys);
 
+//BUT TO CHECK THE EXISTENCE OF A PROPERTY IN AN OBJECT: (IN OPERATOR)
+if ('radius' in circular)
+console.log("The Circular Object already has a radius as a property.");
 
-if ('radius' in circle)
-    console.log('Circle has a radius');
 
 
-// ABSTRACTION: HIDE THE DETAILS AND SHOW THE ESSENTIALS
-function Circulation(radius) {
-    this.radius = radius;
+// ABSTARCATION: A CONCEPT THAT HIDES THE DETIALS/COMPEXITY AND
+// SHOW/EXPOSE THE ESSENTIALS
 
-    this.defaultLocation = {x:0 , y:0};
-    this.computeOptimumLocation = function () {
-        // ...
-    }
+// PRIVATE PROPERTIES AND METHODS
+
+function Shapen(circumference) {
+    this.circumference = circumference;
+
+    let defaultLocation = {x: 0, y: 0};
+    
+    let computeOptimumLocation = function (factor) {
+    // ...
+}
     this.draw = function() {
-        this.computeOptimumLocation();
+        computeOptimumLocation(0.1);
+        // defaultLocation
+        // this.radius
+
         console.log('draw');
     };
 }
-const circulation = new Circulation(10);
-circle.draw();
+
+const shape = new Shapen (200);
+shape.draw();
+
+
+
+
+// GETTERS AND SETTERS
+function square(length) {
+    this.length = length;
+
+    let primaryLocation = {x:10, y:20};
+
+    this.getPrimaryLocation = function() {
+        return primaryLocation
+    }
+    this.draw = function() {
+        console.log('draw');
+    };
+    // GETTER: A FUNCTION USED TO READ A PROPERTY
+
+    Object.defineProperty(this, 'primaryLocation', {
+        get: function() {
+            return primaryLocation;
+        },
+    // A SETTER: A FUNCTION THAT ENABLES USERS TO SET THE VALUES FROM THE OUTSIDE
+
+            // set: function(value) {
+            //     if (!value.x || !value.y)
+            //         throw new Error('Invalid Location');
+
+            //     primaryLocation = value;
+        // }
+    });
+}
+
+const sqr = new square ();
+sqr.primaryLocation = 1;
+sqr.draw();
 
 
 
 
 
+// EXERCISE: A STOPWATCH
+
+function Stopwatch () {
+    let startTime, endTime, running, duration = 0;
+
+this.start = function() {
+    if (running)
+        throw new Error('Stopwatch has already started.');
+    
+    running = true;
+
+    startTime = new Date();
+};
+
+this.stop = function() {
+    if (!running)
+        throw new Error (" Stopwatch has not started.");
+    
+    running: false;
+
+    endTime = new Date();
+
+    const seconds = (endTime.getTime() - startTime.getTime())/1000;
+    duration += seconds;
+};
+
+this.reset = function() {
+    startTime = null;
+    endTime = null;
+    running = false;
+    duration = 0;
+};
+
+Object.defineProperty(this, 'duration', {
+    get:function() {
+        return duration;
+    }
+});
+}
 
 
 
-// PROTOTYPES
-// PROTOTYPICAL INHERITANCE
-// ES6 CLASSES (ECMASCRIPT 6/2015) => CLASSES
+const sw = new Stopwatch();
